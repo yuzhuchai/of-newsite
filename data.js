b991f743b9c869ac1acc9356dc057aede2d242ba
yuzhu chai <yzhuchai@gmail.com>
2022-05-11 13:42:46 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,100 @@
+        let unityVR = {
+        classID: "unityVR",
+        mainPoster:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_bd4d3d5a7f7d93f4dcf2840e20c7b8da?imageView2',
+        mainPosterMobile:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_9f0ac4620469950c8cffb825493d1a82?imageView2',
+        classNameEng:'Master Ccourse Of Virtual Reality With Unity',
+        classNameCn: 'Unity虚拟实境大师班',
+        price:'6999',
+        classSlogan:'搭建次世代影视虚拟场景，掌握最前沿的Metahuman数字人技术!',
+        classHeader:"unity课程大号广告语",
+        classVideo: 'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_v_d41d8cd98f00b204e9800998ecf8427e',
+        videoPoster: 'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_6338d255ebd2efcee45e08b195a7f6f0?imageView2',
+        classSeries:'元宇宙系列课程',
+        classSeriesLink:'',
+        prepClass:'Unity 快速筑基课程',
+        prepClassLink:'',
+        requirements:'Oculus Quest v2三年内主流 PC 或 Mac',
+        classOverview: 'Unity 虚拟实境大师班是在原有基础上进行了重大升级，本课程旨在提供学员从零基础到高阶的全部学习支持和升级路径，在帕森斯设计学院的李肃纲教授的带领下，学员将会深入浅出的掌握 Unity 这门交互软件，成为高级虚拟现实设计师，入职高精尖创新企业，斩获高薪职位。你的脑中有一个幻想的世界你想要把它创造出来与他人分享吗？你想要在虚拟实境里面制作互动玩乐体验又又苦于没有硬核编程背景吗？你想尝试把你设计背景的内容转换成虚拟实境体验的原型吗？那么快加入这门课程吧',
+        syllabus: [
+            {
+                title: 'Styly 入门',
+                description: '掌握 styly 线上编辑器，并能够使用 styly 创建一个虚拟场景，为后期学习做准备',
+                img:"http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_5abacbac97ecbf0cd37938df92d34028?imageView2"
+            },
+            {
+                title:'VR简介与空间准备',
+                description:'快速入门 Uniy VR ,搭建一个 WhiteBoxing 场景，了解 VR 发展历史',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_de2faf658af13ed8e13ee430f7c1f6e0?imageView2'
+            },
+            {
+                title:'灯光渲染与后期制作',
+                description:'掌握基本灯光渲染手段和最有效的灯光渲染技术，和 Post Processing Stack 技术为',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_01024513886bdf754bd949a7bb960f23?imageView2'
+            },
+            {
+                title:'互动场景搭建',
+                description:'掌握 playmaker 可视化脚步的核心概念- 有限状态机，完成互动场景',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_d307267fb2ee54076ce1263e1ff80c10?imageView2'
+            },
+            {
+                title:'互动剧情设计',
+                description:'使用 Niagara system ，掌握其基础概念。学会高级粒子系统以及触发粒子的方式',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_2e668456fe97a86d514a783e01d3af42?imageView2'
+            },
+            {
+                title: "VFX特效与资产管理",
+                description: '使用 VFX graph 制作粒子效果，在场景中添加雨/雪/火焰等特效',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_47781cd7383316c0b80f5521bd1f4d1c?imageView2'
+            },
+            {
+                title: 'VR眼镜与XR体验',
+                description: '掌握 Unity 中 XR 模式设定，如何将作品上传到 VR 眼镜中，并完成一个超体验',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_234708551f227242592b6a279c939eab?imageView2'
+            },
+            {
+                title:'总结 & 作品发布',
+                description:'总结课程，拓展学习如何使用 VR/AR 虚拟媒体工具跨平台发布作品到各大 VR 平台',
+                img:'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_61640ab2507efef78425b33e354b9e7d?imageView2'
+            }
+        ],
+
+        showCase: ["http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_ea50ef589cbd97ce394acccbebbf981a?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_7b6be75398f1d7f03fc3b90f7702202f?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_61640ab2507efef78425b33e354b9e7d?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_917aa7ec30fb13c59b77e20eebc49d53?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_90d98a12c3134024b4f126afe65acb6e?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_77ad1b8c09047d73740521c7e90d86d1?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_76158be46f03ea05fdc59dcf62a4e916?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_253095f491a75a9c588bbb6bef3d1ebc?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_118988a94b8dfd299661753419e482ad?imageView2",
+                    "http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_5c9995b80cbc1a11879374e6f3e888f3?imageView2"],
+        mentorImg: 'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_e3add361a84d3477ce312f6f923c48e8?imageView2',
+        mentorNameEng:'Kyle Li',
+        mentorNameCn:'李肃纲',
+        mentorTitle:["帕森斯设计学院副教授研究生导师","帕森斯设计与科技前系主任","STYLY官方讲师"],
+        mentorText:'授课细致耐心，实战经验丰富，擅于总结，擅长将一线工作流程融入课堂，运用深入浅出的教学方法，从原理讲解到一通百通，逐步让你成为大佬',
+        mentorVideo:"",
+        freeClassTitle:'Styly VR网页编辑器创作技巧',
+        freeClassLink:'',
+        teacherCase:['http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_0bc3518d1ecbb37b80389fc699d454c6?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_8df648f91d78a3ec520c6bda9e611690?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_34258a2e0a479f3f7e3f26701c8a0d65?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_1ebe7cb5fcd74cd16ae89b39a31aa23e?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_f7f017a9e5f8a11c2b795cbff258d7c3?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_ba03492fd53df18425bee6113de717ce?imageView2'],
+        studentWork: ['http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_dadf8069b128184d7b72c4a6ada72ad4?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_61af4ea1404a9e85599c8ca00e2fffb0?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_6f9a2de7acfb31588254761010537c75',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_58c270024c5a465f47ab75d2246d9648?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_eda74a3ab2889a4ccf3b5b5ce3470f2b?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_1aed213cc7c6905394ac7988c16a9965?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_2cf3a3d80e9c2a9a7b49f07c67a85156?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_f3cecd30f7a7b1667625adade7988ca1?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_02de1a921b9fa53dd8e7dfbb0f608456?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_ce4c46e019a63cfd1d9d9ea62508ceaa?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_a70b20cc40bd67cc506801e9988b5d51?imageView2',
+                    'http://cdn0.ofcourse.io/5b1ca0cb349f4250877dbc4fc16fc1e8_5e338c6b05cb46709abff6f4181bb1e2?imageView2'
+                    ],
+    }
+
+    
